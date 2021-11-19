6b7d7c3f0ed278fd1d15b6a41c7f21d229f5c8e0
khoa2520 <86915350+khoa2520@users.noreply.github.com>
2021-11-19 21:30:15 +0700

    updateframe

---
+++ asset/javascrip/header.js
@@ -0,0 +1,14 @@
+function user_block_open() {
+    // if (document.getElementById('user-block').style.display == 'none') {
+    //     document.getElementById('user-block').style.display = 'block';
+        
+
+    // }
+    // else {
+    //     document.getElementById('user-block').style.display = 'none'
+    // }
+    document.getElementById('user-block').style.display = 'block';
+}
+function user_block_close(){
+    document.getElementById('user-block').style.display = 'none'
+}
