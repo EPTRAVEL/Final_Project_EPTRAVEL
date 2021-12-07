0bbb4c45ac2ddff3ffbdf739dce79cf2ae6d5b31
khoa2520 <86915350+khoa2520@users.noreply.github.com>
2021-12-07 14:40:48 +0700

    ypdate header

--- asset/javascrip/header.js
+++ asset/javascrip/header.js
@@ -18,3 +18,18 @@ function user_block_close(){
         
     // }, 5000);
 }
+
+function hamburger_o(){
+    document.getElementById('nav_id').style.display = 'block';
+    document.getElementById('hamburger-o').style.display = 'none';
+    document.getElementById('hamburger-x').style.display = 'block';
+
+    
+}
+function hamburger_x(){
+    document.getElementById('nav_id').style.display = 'none';
+    document.getElementById('hamburger-o').style.display = 'block';
+    document.getElementById('hamburger-x').style.display = 'none';
+
+    
+}
