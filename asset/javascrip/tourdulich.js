3d22fcc1392b88755fdaeacadb612cf17d090708
thititing123 <93417411+thititing123@users.noreply.github.com>
2021-12-12 10:25:00 +0700

    tour

---
+++ asset/javascrip/tourdulich.js
@@ -0,0 +1,12 @@
+function hamburger_o(){
+    document.getElementById('select').style.display = 'block';
+    document.getElementById('hien-o').style.display = 'none';
+    document.getElementById('an-x').style.display = 'block';
+
+    
+}
+function hamburger_x(){
+    document.getElementById('select').style.display = 'none';
+    document.getElementById('hien-o').style.display = 'block';
+    document.getElementById('an-x').style.display = 'none';
+}
