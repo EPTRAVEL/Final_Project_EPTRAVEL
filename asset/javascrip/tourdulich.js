0a9dafb4ca86837d4eb462ef9e580fa0dee72ba9
thititing123 <93417411+thititing123@users.noreply.github.com>
2021-12-23 14:53:12 +0700

    sort js

--- asset/javascrip/tourdulich.js
+++ asset/javascrip/tourdulich.js
@@ -107,33 +107,7 @@ function myFunction(arr) {
   document.getElementById("category").innerHTML = div;
 }
 //sắp xếp tour
-function ascending() {
-    document.getElementById("category").innerHTML = "";
-  var xmlhttp = new XMLHttpRequest();
-  var url = "asset/data/tour.json";
-  xmlhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      var myArr = JSON.parse(this.responseText);
-      ascending_2(myArr);
-    }
-  };
-  xmlhttp.open("GET", url, true);
-  xmlhttp.send();
-  function ascending_2(arr) {
-    let i = document.getElementById("sapxep-select").value;
-    arr = arr.sort(function (a, b) {
-      if (i == "thap-cao") {
-        return a.giatiennguoilon - b.giatiennguoilon;
-      } else if (i == "cao-thap") {
-        return b.giatiennguoilon - a.giatiennguoilon;
-      } else if(i =='theogiamgia') {
-        return b.giamgia - a.giamgia
-      }
-    });
-    // document.getElementById("category").innerHTML = "";
-    myFunction(arr);
-  }
-}
+
 function myFunction_loc(arr) {
   // arrKhuVuc = [a,b];
   // arrDiemDen = [];
