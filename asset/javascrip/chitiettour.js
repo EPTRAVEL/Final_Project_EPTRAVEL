a65cec0966f7cd81d94afd6f6d854bcad5c16fa9
thititing123 <93417411+thititing123@users.noreply.github.com>
2021-12-25 21:49:26 +0700

    chitiettour

--- asset/javascrip/chitiettour.js
+++ asset/javascrip/chitiettour.js
@@ -1,3 +1,9 @@
+function goThongTin(matour) {
+    window.location.href = "detail.html?matour=" + matour;
+}
+function goThongtin(id) {
+    window.location.href = "thongtin.html?matour=" + id;
+}
 var xmlhttp = new XMLHttpRequest();
 var url = "asset/data/tour.json";
 xmlhttp.onreadystatechange = function () {
@@ -15,12 +21,18 @@ function myFunction(arr) {
         if (arr[i].ma_tour == matour) {
             div += "<div class='container-x1'><div class='row--tour'><div class='row--tour__left'><p class='tour-item__code'>Mã tour: <b>" +
                 arr[i].ma_tour + "</b></p><h3 class='tour-item__title'>" +
-                arr[i].ten_tour + "</h3><div class='short-rating'><div class='rating'><span id='icon'><img src='./asset/images/ICON/heart_w.png' alt='tym'></span><label>100</label></div><button style='z-index: 10;' onclick='themyeuthich_chitiettour(this)' class='yeuthich'><span id='icon'><img src='./asset/images/ICON/plus.png'alt='plus'></span> Thêm vào yêu thích</button></div></div><div class='row--tour__right'><div class='tour-item__price--current__number'><span id='tour-item__price--current__number'>" +
-                formatNumber(arr[i].giatiennguoilon) + "đ</span>/Khách</div><div class='tour-item__price--addcart'><button class='btn btn-book'><span id='icon'><img src='./asset/images/ICON/cart.png' alt='Đặt hàng'></span>Đặt ngay</button><button class='btn btn-block'>Liên hệ tư vấn</button></div></div></div></div><div class='tour--detail'><div class='row--tour--img'><div class='tour--img__first'><img id='img-tour' src='" +
-                arr[i].images[2] + "' alt='YT'></div><div class='tour--img__last'><div class='tour--img'><div class='img margin-img' style='margin-right: 10px;'><img id='img-tour' src='" +
-                arr[i].images[3] + "' alt='BD'></div><div class='img'><img id='img-tour' src='" +
-                arr[i].images[4] + "' alt='VHL'></div></div><div class='tour--img' style='margin-top:10px'><img id='img-tour' src='" +
-                arr[i].images[5] + "' alt='TA'></div></div></div><div class='row--content'><div class='motatour--content'>" +
+                arr[i].ten_tour + "</h3><div class='short-rating'><div class='rating'><span id='icon'><img src='./asset/images/ICON/heart_w.png' alt='tym'></span><label>100</label></div><button onclick='themyeuthich_chitiettour(this)' class='yeuthich'><span id='icon'><img src='./asset/images/ICON/plus.png'alt='plus'></span> Thêm vào yêu thích</button></div></div><div class='row--tour__right'><div class='tour-item__price--current__number'><span id='tour-item__price--current__number'>" +
+                formatNumber(arr[i].giatiennguoilon) + "đ</span>/Khách</div><div class='tour-item__price--addcart'><button  class='btn btn-book' onclick=" +
+                "goThongtin(" +
+                "'" +
+                arr[i].ma_tour +
+                "'" +
+                ")" +
+                "><span id='icon'><img src='./asset/images/ICON/cart.png' alt='Đặt hàng'></span>   Đặt ngay</button><button class='btn btn-block'>Liên hệ tư vấn</button></div></div></div></div><div class='tour--detail'><div class='row--tour--img'><div class='tour--img__first'><img id='img-tour' src='" +
+                arr[i].images[1] + "' alt='YT'></div><div class='tour--img__last'><div class='tour--img'><div class='img margin-img' style='margin-right: 10px;'><img id='img-tour' src='" +
+                arr[i].images[2] + "' alt='BD'></div><div class='img'><img id='img-tour' src='" +
+                arr[i].images[3] + "' alt='VHL'></div></div><div class='tour--img' style='margin-top:10px'><img id='img-tour' src='" +
+                arr[i].images[4] + "' alt='TA'></div></div></div><div class='row--content'><div class='motatour--content'>" +
                 arr[i].noidungchitiet + "</div><div class='overviewtour--content'><div class='box-order'><div class='time'><p>Ngày khởi hành: <b>" +
                 arr[i].ngaykhoihanh + "</b></p><p>Thời gian: <b>" +
                 arr[i].thoigian + "</b></p><p>Nơi khởi hành: <b> " +
@@ -37,23 +49,28 @@ function myFunction(arr) {
 }
 var yeuthich = new Array();
 function themyeuthich_chitiettour(x) {
-    //Lấy ID bằng click  -> DOM
-    var id = x.parentElement.parentElement.children[0].children[0].innerText;
-    // alert(id);
-    var yt = new Array(id);
-    var title = x.parentElement.parentElement.children[1].innerText; //Lấy title
-    // alert(title);
-    for (var i = 0; i < yeuthich.length; i++) {
-        if (yeuthich[i][0] == id) {
-            return alert("Tour" + title + " đã có sẵn trong danh mục yêu thích");
+    if (localStorage.getItem("User_login") === null) {
+        alert("Cần phải đăng nhập trước khi thêm vào Yêu Thích");
+        document.getElementById("modal__container").style.display = "flex";
+    } else {
+        //Lấy ID bằng click  -> DOM
+        var id = x.parentElement.parentElement.children[0].children[0].innerHTML;
+        // alert(id);
+        var yt = new Array(id);
+        var title = x.parentElement.parentElement.children[1].innerText; //Lấy title
+        // alert(title);
+        for (var i = 0; i < yeuthich.length; i++) {
+            if (yeuthich[i][0] == id) {
+                return alert("Tour" + title + " đã có sẵn trong danh mục yêu thích");
+            }
         }
+        yeuthich.push(yt);
+        alert(title + " đã được thêm vào yêu thích");
+
+        //Lưu giỏ hàng
+        sessionStorage.setItem("yeuthich", JSON.stringify(yeuthich));
     }
-    yeuthich.push(yt);
-    alert(title + " đã được thêm vào yêu thích");
-  
-    //Lưu giỏ hàng
-    sessionStorage.setItem("yeuthich", JSON.stringify(yeuthich));
-  }
+}
 
 
 
